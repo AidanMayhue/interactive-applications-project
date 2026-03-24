f35f70a86a601da34fb855ec81b1eb7f30e88bba
Aidan Mayhue <144569383+AidanMayhue@users.noreply.github.com>
2026-03-24 19:44:47 -0400

    improving layout

---
+++ pages/visualizations.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Analytics")
+
+st.header("Average temperature over last 30 days (line chart)")
+
+
+st.header("Outfit category percentage breakdown (pie chart)")
+##ie. formal 20%, casual 30%, beachwear 50%
