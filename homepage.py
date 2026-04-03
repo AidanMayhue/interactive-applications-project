cf80ff01ebe7feee9256595e91c7f97dd6823383
Carissa <jac7az@gmail.com>
2026-04-03 00:03:06 -0400

    Add files via upload

--- homepage.py
+++ homepage.py
@@ -5,3 +5,7 @@ st.set_page_config(
 )
 
 st.title("OutfitPlannerz")
+
+st.subheader("Today's Outfit: Khaki pants, button-up shirt, and brown boots")
+st.write("Today's Weather: Sunny, 85°F")
+st.write("No events scheduled for today. Check your calendar for upcoming events and outfit recommendations!")
